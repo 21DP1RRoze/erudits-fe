8438282d897f427d02e7207c2d32d7b591020f92
Gundars Rēbuks <rebuks.gundars@gmail.com>
2024-03-27 03:33:38 +0200

    Quiz instance creation

--- src/InstanceCard.jsx
+++ src/InstanceCard.jsx
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react';
 import API from './axiosApi';
 import { useNavigate } from 'react-router-dom';
 
-const InstanceCard = ({quiz}) => {
+const InstanceCard = ({ instance }) => {
 
     const navigate = useNavigate();
     const [loggedIn, setLoggedIn] = useState(false);
@@ -23,9 +23,9 @@ const InstanceCard = ({quiz}) => {
         <div className="col-lg-4">
             <div className="glass card mb-3">
                 <div className="card-body">
-                    <h5 className="card-title">{quiz.title}</h5>
-                    <p className="card-text">{quiz.description}</p>
-                    <button onClick={() => navigate("/game/1")} className="urbanist p-1 ps-3 pe-3 btn-action">Pieslēgties</button>
+                    <h5 className="card-title">{instance.quiz.title}</h5>
+                    <p className="card-text">{instance.quiz.description}</p>
+                    <button onClick={() => navigate(`/game/${instance.id}`)} className="urbanist p-1 ps-3 pe-3 btn-action">Pieslēgties</button>
                     {loggedIn && <i className="fa-solid fa-pen-to-square editInstanceButton" ></i>}
                 </div>
             </div>
